b67b7873bdadb84428832faba7a48959d8cea80b
mercy <mercyjemosop@gmail.com>
2023-06-01 17:12:35 +0300

    Test

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 const bodyParser = require('body-parser');
 const axios = require('axios'); // Add axios import
 
-
-
 const app = express();
 app.use(express.json());
 app.use(cors()); // Add CORS middleware
